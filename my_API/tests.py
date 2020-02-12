63e949f101ba7a9c5311896c39111b22db313cbd
Tomasz Binkiewicz <t.bin44@gmail.com>
2020-02-13 00:09:59 +0100

    test_case_for_language_api_view

--- my_API/tests.py
+++ my_API/tests.py
@@ -7,7 +7,7 @@ class AllLanguagesAPITestCase(APITestCase):
 
     def test_create_language(self):
         url = '/API/languages'
-        data = {"language": "en"}
+        data = {'language': 'en'}
         response = self.client.post(path=url, data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Language.objects.count(), 1)
@@ -25,6 +25,33 @@ class AllLanguagesAPITestCase(APITestCase):
         Language.objects.create(language='en')
         Language.objects.create(language='pl')
         url = '/API/languages'
+        response = self.client.delete(path=url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Language.objects.count(), 0)
+
+
+class LanguageAPITestCase(APITestCase):
+
+    def test_display_language(self):
+        url = '/API/languages/1'
+        response = self.client.get(path=url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        Language.objects.create(language='pl')
+        response = self.client.get(path=url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'id': 1, 'language': 'pl'})
+
+    def test_update_language(self):
+        Language.objects.create(language='pl')
+        url = '/API/languages/1'
+        data = {'language': 'en'}
+        response = self.client.put(path=url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Language.objects.get().language, 'en')
+
+    def test_delete_language(self):
+        Language.objects.create(language='pl')
+        url = '/API/languages/1'
         response = self.client.delete(path=url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Language.objects.count(), 0)
